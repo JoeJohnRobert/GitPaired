dc1c0d09dd8b067b7f0b563c554a2b13e89b46de
JoeJohnRobert <josephcardarelli@gmail.com>
2014-12-05 15:20:29 -0500

    workin on collab button

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,8 +3,15 @@ class ProjectsController < ApplicationController
   def update
     @user = User.find(session[:user_id])
     @project = Project.find(params[:id])
-    @project.update(:collaborator_wanted => params[:collaborator_wanted])
+    @project.update(project_params)
     redirect_to user_path(@user)
   end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:user_id, :collaborator_wanted)
+  end
+
 
 end
