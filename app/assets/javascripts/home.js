0326d7a1dfc09e00f900cfac83d3e583f5c56692
strohy120 <sarah.stroh@flatironschool.com>
2014-12-05 18:09:50 -0500

    fix accept collaborators button

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,8 @@
 $(function(){
+//it should start grey if true it should start ble
 
-
+  $('ol').on('click', 'li', function(){
+    $(this).find('.fa').toggleClass('blue');
+  })
 
 });
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,8 +3,16 @@ class ProjectsController < ApplicationController
   def update
     @user = User.find(session[:user_id])
     @project = Project.find(params[:id])
-    @project.update(:collaborator_wanted => params[:collaborator_wanted])
-    redirect_to user_path(@user)
+
+    if params[:project][:collaborator_wanted] == 'toggle'
+      if @project.collaborator_wanted == true
+        @project.update(:collaborator_wanted => false)
+      else
+        @project.update(:collaborator_wanted => true)
+      end
+    end
+    # @project.update(:collaborator_wanted => params[:collaborator_wanted])
+    # redirect_to user_path(@user)
   end
 
 end
