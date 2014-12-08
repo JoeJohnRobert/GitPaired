9dfc12f5d2d861c0eebc1986ffc0e233b0d1a5d8
JoeJohnRobert <josephcardarelli@gmail.com>
2014-12-08 18:17:14 -0500

    calc gmap route on user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @curr_user = current_user
     @full_name = @user.first_name + " " + @user.last_name
     if @user.zipcode
       @neighborhood = @user.get_neighborhood
@@ -9,10 +10,10 @@ class UsersController < ApplicationController
     if current_user
       coords_array = Geocoder::Calculations.geographic_center(["#{@user.zipcode}", "#{current_user.zipcode}"])
       @coords_hash = {latitude: coords_array[0], longitude: coords_array[1]}
+      search_params = {term: 'wifi', category_filter: 'coffee', radius_filter: 1609, limit: 2, sort: 2}
+      response = YELP.search_by_coordinates(@coords_hash, search_params)
+      @wifi_spots = response.businesses.map {|biz| [biz.name, biz.rating, biz.url, biz.image_url]}
     end
-    search_params = {term: 'wifi', category_filter: 'coffee', radius_filter: 1609, limit: 2, sort: 2}
-    response = YELP.search_by_coordinates(@coords_hash, search_params)
-    @wifi_spots = response.businesses.map {|biz| [biz.name, biz.rating, biz.url, biz.image_url]}
   end
 
   def update
