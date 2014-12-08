cf20e399781b8d8fb652c52a183ff233497647f0
strohy120 <sarah.stroh@flatironschool.com>
2014-12-08 18:06:17 -0500

    form works

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,20 +4,9 @@ class ProjectsController < ApplicationController
 
     @user = User.find(session[:user_id])
     @project = Project.find(params[:id])
-# binding.pry
-    @check= params[:project][:collaborator_wanted] 
-    if params[:project][:collaborator_wanted] && params[:project][:collaborator_wanted] == 'toggle'
-      if @project.collaborator_wanted == true
-        @project.update(:collaborator_wanted => false)
-      else
-        @project.update(:collaborator_wanted => true)
-      end
-    end
 
-    if params[:project][:description]
-   
-      @project.update(description: params[:project][:description], need_help_with: params[:project][:need_help_with])
-    end
+      @project.update(description: params[:project][:description], need_help_with: params[:project][:need_help_with], collaborator_wanted: params[:collaborator_wanted])
+
 
   end
 
