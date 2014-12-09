e5b03ade24aa68c19b840ab1f97e48cbf2583061
JoeJohnRobert <josephcardarelli@gmail.com>
2014-12-09 14:16:51 -0500

    fix search bug

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,22 +1,18 @@
 class SearchesController < ApplicationController 
 
   def index
-    
     @query = params[:query]
     @radius = 100 #measured in miles
     @users = User.select_nearby_users(current_user, @query, @radius)
     @users.each { |u| @projects = u.all_projects }
     @language_filters = @projects.map {|p| p.language}.uniq
     @fade_options = ['fade-right d1','fade-right','fade_left','fade-left d1']
-
-    if @users.size > 0
-      render :index
-    elsif @query[(/\d{5}/)] == @query
+    if @query[(/\d{5}/)] == @query && @user.size == 0
       redirect_to root_path, notice: "Sorry, we don't have any users yet in that zip!"
-    else 
-      redirect_to root_path, notice: "No results"
+    elsif @query[(/\d{5}/)] != @query
+      redirect_to root_path, notice: "Invalid zip code."
+    else
+      render :index
     end
   end  
-
-
 end
