7bbb1da224fc3068f41d829c1f3db3cc0649feb5
kevinhylant <kevin.hylant@gmail.com>
2014-12-09 14:34:35 -0500

    fix search bugs in search controller and fix languages for search results page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,9 +4,17 @@ class SearchesController < ApplicationController
     @query = params[:query]
     @radius = 100 #measured in miles
     @users = User.select_nearby_users(current_user, @query, @radius)
-    @users.each { |u| @projects = u.all_projects }
-    @language_filters = @projects.map {|p| p.language}.uniq
+    if @users.size > 0
+      @projects = (@users.collect {|u| u.all_projects}).flatten
+    end
+    if @projects
+      @language_filters = (@projects.map {|p| p.language}).uniq
+    else
+      @language_filters = []
+    end
+
     @fade_options = ['fade-right d1','fade-right','fade_left','fade-left d1']
+
     if @query[(/\d{5}/)] == @query && @users.size == 0
       redirect_to root_path, notice: "Sorry, we don't have any users yet in that zip!"
     elsif @query[(/\d{5}/)] != @query
