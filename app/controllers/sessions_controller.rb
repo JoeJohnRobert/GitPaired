fd9337cb406539feaf2eff3eda365b982cf67c4f
kevinhylant <kevin.hylant@gmail.com>
2014-12-08 20:47:05 -0500

    prep to pull

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   after_action :set_origin_path
 
-
   def create
     auth_hash = request.env["omniauth.auth"]
     user = User.get_user_from_omniauth(auth_hash)
@@ -12,13 +11,14 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    reset_session
-    redirect_to root_path
+    # reset_session
+    session[:user_id] = nil
+    redirect_to session[:origin_path]
   end
 
 
   private
     def set_origin_path
-      # session[:origin_path] = request.path
+      session[:origin_path] = request.path
     end
 end
