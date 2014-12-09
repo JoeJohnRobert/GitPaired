a8f408ed23c90118d9609bce02b303cc8c303540
JoeJohnRobert <josephcardarelli@gmail.com>
2014-12-09 14:19:35 -0500

    fix search typo

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,7 +7,7 @@ class SearchesController < ApplicationController
     @users.each { |u| @projects = u.all_projects }
     @language_filters = @projects.map {|p| p.language}.uniq
     @fade_options = ['fade-right d1','fade-right','fade_left','fade-left d1']
-    if @query[(/\d{5}/)] == @query && @user.size == 0
+    if @query[(/\d{5}/)] == @query && @users.size == 0
       redirect_to root_path, notice: "Sorry, we don't have any users yet in that zip!"
     elsif @query[(/\d{5}/)] != @query
       redirect_to root_path, notice: "Invalid zip code."
