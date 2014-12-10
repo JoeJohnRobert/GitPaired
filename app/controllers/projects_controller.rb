95c99e517c890f056044b9a0a47a7f1221f35e18
JoeJohnRobert <josephcardarelli@gmail.com>
2014-12-09 19:56:25 -0500

    trying to fix but with ajax form and collab wanted not being saved because collab wanted not on the same hash level of params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,10 +4,11 @@ class ProjectsController < ApplicationController
     @user = User.find(session[:user_id])
     @project = Project.find(params[:id])
     @project.update(project_params)
+    @project.update(collaborator_wanted: params[:collaborator_wanted]) #collab wanted not on the same hash level
   end
 
   private
     def project_params
-      params.require(:project).permit(:user_id, :collaborator_wanted, :description, :need_help_with)
+      params.require(:project).permit(:user_id, :description, :need_help_with)
     end
 end
