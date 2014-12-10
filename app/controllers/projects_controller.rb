193c59a78f561419254b33b758f1b3026e5a5f21
JoeJohnRobert <josephcardarelli@gmail.com>
2014-12-10 12:12:17 -0500

    fix collab button

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,11 +4,18 @@ class ProjectsController < ApplicationController
     @user = User.find(session[:user_id])
     @project = Project.find(params[:id])
     @project.update(project_params)
-    @project.update(collaborator_wanted: params[:collaborator_wanted]) #collab wanted not on the same hash level of params
+    # @project.update(collaborator_wanted: params[:collaborator_wanted]) #collab wanted not on the same hash level of params
+    if params[:collaborator_wanted] == 'toggle'
+      if @project.collaborator_wanted == true
+        @project.update(:collaborator_wanted => false)
+      else
+        @project.update(:collaborator_wanted => true)
+      end
+    end
   end
 
   private
     def project_params
-      params.require(:project).permit(:user_id, :description, :need_help_with)
+      params.require(:project).permit(:user_id, :description, :need_help_with, :collaborator_wanted)
     end
 end
